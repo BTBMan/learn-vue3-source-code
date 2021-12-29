d3b269bd735254e0d3777133b32c813d613990e2
BTBMan <1498118157@qq.com>
2021-12-29 22:07:57 +0800

    readonly理解中

--- src/reactivity/src/baseHandlers.ts
+++ src/reactivity/src/baseHandlers.ts
@@ -1,10 +1,10 @@
-import { ReactiveFlags, reactiveMap } from './reactive';
+import { ReactiveFlags, reactiveMap, readonly, readonlyMap } from './reactive';
 import { isObject } from '../../shared';
 import { reactive } from '.';
 
-const set = createSetter(); // 每一种reactive类型的setter逻辑都是一样的
-// 然而不同的reactive类型的getter逻辑是有区别的
+const set = createSetter();
 const get = createGetter();
+const readonlyGet = createGetter(true);
 
 // 创建proxy的getter和setter方法 这里抽离出来的目的是为了方便创建其他类型的reactive劫持
 function createGetter(isReadonly = false, shallow = false) {
@@ -12,21 +12,28 @@ function createGetter(isReadonly = false, shallow = false) {
     // 这里是处理特殊属性的getter (是否是响应或只读数据 或者获取被代理的原始数据)
     const isExistInReactiveMap = () =>
       key === ReactiveFlags.IS_RAW && receiver === reactiveMap.get(target);
+    const isExistInReadonlyMap = () =>
+      key === ReactiveFlags.IS_RAW && receiver === readonlyMap.get(target);
 
     if (key === ReactiveFlags.IS_REACTIVE) {
       return !isReadonly;
-    } else if (isExistInReactiveMap()) {
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    } else if (isExistInReactiveMap() || isExistInReadonlyMap()) {
       return target;
     }
 
     // 如果访问的不是特殊属性的话 以下为处理正常的数据
-
     const res = Reflect.get(target, key, receiver);
+
+    // readyonly类型的数据是不会出发依赖的 所以不用收集
+    if (!isReadonly) {
+      // TODO 依赖收集操作
+    }
 
     if (isObject(res)) {
       // 这里的作用是 当访问一个嵌套的对象的话 再次把它变为响应式的数据 并且返回
-      // isReadonly ?
-      return reactive(res);
+      return isReadonly ? readonly(res) : reactive(res);
     }
 
     return res;
@@ -41,8 +48,19 @@ function createSetter() {
   };
 }
 
-// 这个用来处理我们正常创建的reactive的劫持
+// 这个用来处理我们创建正常的reactive的劫持
 export const mutableHandlers = {
   get,
   set,
+};
+
+// 这个用来处理我们创建的readonly的劫持
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set(target, key) {
+    // readonly的值不可以被更改
+    console.warn(`Set operation on key "${String(key)}" failed: target is readonly.`, target);
+
+    return true;
+  },
 };
--- src/reactivity/src/reactive.ts
+++ src/reactivity/src/reactive.ts
@@ -1,6 +1,7 @@
-import { mutableHandlers } from './baseHandlers';
+import { mutableHandlers, readonlyHandlers } from './baseHandlers';
 
-export const reactiveMap = new WeakMap(); // 用来缓存已经创建过的reactive
+export const reactiveMap = new WeakMap(); // 用来缓存已经创建过的正常的reactive
+export const readonlyMap = new WeakMap(); // 用来缓存已经创建过的readonly
 
 // 建立一组枚举 这些是用来存放被代理过后的对象内部的特有的属性 一般用来判断是否是响应或只读数据 或者被代理的原始的数据的
 export const enum ReactiveFlags {
@@ -9,9 +10,16 @@ export const enum ReactiveFlags {
   IS_RAW = '__v_raw',
 }
 
+// 创建一个正常的reactive数据
 export function reactive(target) {
   // 当用户使用reactive的时候 就会返回一个创建的reactive的对象
   return createReactiveObject(target, reactiveMap, mutableHandlers);
+}
+
+// 创建一个readonly类型的数据
+export function readonly(target) {
+  // 当用户使用readonly的时候 就会返回一个创建的readonly的对象
+  return createReactiveObject(target, readonlyMap, readonlyHandlers);
 }
 
 // 创建reactive对象的方法单独抽离 以便共用逻辑
