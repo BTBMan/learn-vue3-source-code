38be28bb8dfee8253fbb02dd9563c34d8dc76902
BTBMan <1498118157@qq.com>
2021-12-29 21:49:19 +0800

    getters部分实现

--- src/reactivity/__test__/reactive.spec.ts
+++ src/reactivity/__test__/reactive.spec.ts
@@ -4,10 +4,18 @@ describe('reactive', () => {
   test('test', () => {
     expect(1 + 1).toBe(2);
 
-    const obj = { a: 'a' };
+    const obj = {
+      a: 'a',
+      b: {
+        c: 'c',
+      },
+    };
 
     const data1 = reactive(obj);
 
-    data1.a = 'b';
+    // data1.a = 'b';
+
+    data1.b.c;
+    data1.b.c;
   });
 });
--- src/reactivity/src/baseHandlers.ts
+++ src/reactivity/src/baseHandlers.ts
@@ -1,4 +1,6 @@
 import { ReactiveFlags, reactiveMap } from './reactive';
+import { isObject } from '../../shared';
+import { reactive } from '.';
 
 const set = createSetter(); // 每一种reactive类型的setter逻辑都是一样的
 // 然而不同的reactive类型的getter逻辑是有区别的
@@ -7,12 +9,27 @@ const get = createGetter();
 // 创建proxy的getter和setter方法 这里抽离出来的目的是为了方便创建其他类型的reactive劫持
 function createGetter(isReadonly = false, shallow = false) {
   return function (target, key, receiver) {
-    // 首先去处理特殊属性的getter (是否是响应或只读数据 或者获取被代理的原始数据)
-    // 首先去找当前对象有没有被缓存过
+    // 这里是处理特殊属性的getter (是否是响应或只读数据 或者获取被代理的原始数据)
     const isExistInReactiveMap = () =>
       key === ReactiveFlags.IS_RAW && receiver === reactiveMap.get(target);
 
-    return Reflect.get(target, key, receiver);
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if (isExistInReactiveMap()) {
+      return target;
+    }
+
+    // 如果访问的不是特殊属性的话 以下为处理正常的数据
+
+    const res = Reflect.get(target, key, receiver);
+
+    if (isObject(res)) {
+      // 这里的作用是 当访问一个嵌套的对象的话 再次把它变为响应式的数据 并且返回
+      // isReadonly ?
+      return reactive(res);
+    }
+
+    return res;
   };
 }
 
